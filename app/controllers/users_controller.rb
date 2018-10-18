258e53de750bfaf401f813bb0244d4864a06bc3d
kousuke.nakano <ikikewawimps1201@ezweb.ne.jp>
2018-10-18 07:54:49 +0000

    likes-add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,12 @@ class UsersController < ApplicationController
     counts(@user)
   end
   
+  def likes
+    @user = User.find(params[:id])
+    @microposts = @user.like_microposts.order('created_at DESC').page(params[:page])
+    counts(@user)
+  end
+  
   private
 
   def user_params
